ea8201d3eb29309578df6313362ae736b0dea630
witoldzol <blue3656@gmail.com>
2018-05-05 21:38:55 +0100

    updated create account resource, implemented account number generators, created account creation method in client app

--- client_bankAPI/script.js
+++ client_bankAPI/script.js
@@ -1,23 +1,40 @@
 //helper function
 let cl = x=>console.log(x)
-//uri to resource
-let uri = "http://localhost:49000/banking/customers"
-//ele is used in ajax call to build html elements
-let ele
+//
+let uri="http://localhost:49000/banking/customers"
+//currently logged in user
+let currentUser
 //standarized settings for ajax call
 let settings = {
   "async": true,
   "crossDomain": true,
-  "url": uri,
+  "url": "http://localhost:49000/banking/customers",
   "method": "GET",
   "headers": {
     "Cache-Control": "no-cache",
   }
 }
 
+//helper function
+function resetSettings(){
+    settings ={}
+    settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": uri,
+      "method": "GET",
+      "headers": {
+        "Cache-Control": "no-cache",
+      }
+    }
+}
 
 //standarized api call, takes path to resource and function to execute on api response
 function apiCall(uri, method){
+    //update uri
+    settings.url = uri
+    cl(settings.url)
+    //make a call
     $.ajax(settings)
         .done(response=>{method(response)})
 }
@@ -26,8 +43,8 @@ function apiCall(uri, method){
 //listener
 $('#login').click( login )
 
-function login(uri){
-    let status = false;
+function login(){
+    uri = "http://localhost:49000/banking/customers"
     //get login and password from the form
     userName = $('#loginName').val()
     password = $('#password').val()
@@ -36,8 +53,6 @@ function login(uri){
 }
 // function that return map that traverses json and compares input to values
 function mapThrough(response){
-    cl('from map throu function ' + response)
-    cl(response.loginName)
      response.map( obj=>{
                     if(obj.loginName == userName && obj.password == password){
                         enter(obj)
@@ -46,6 +61,7 @@ function mapThrough(response){
 }
 //make login forms visible upon logging in
 function enter(obj){
+    currentUser = obj;
     $('#inputsForm').css("visibility", "visible")
     $('#greeting').html('welcome ' + obj.name)
 }
@@ -60,6 +76,8 @@ function newUserMakeFormsVisible(){
 }
 //create new user
 function createNewUser(){
+    //define uri
+    uri="http://localhost:49000/banking/customers"
     //get data from input forms
     let name = $('#newUserName').val()
     let address = $('#newUserAddress').val()
@@ -83,18 +101,81 @@ function createNewUser(){
     $('#password').val(password)
     
     //reset settings
-    settings ={}
-    settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": uri,
-      "method": "GET",
-      "headers": {
-        "Cache-Control": "no-cache",
-      }
-    }
+    resetSettings()
 
 }
+
+// ----------------- GET ALL ACCOUNTS for user
+//listener
+$('#listAccounts').click( listAccounts )
+//list accounts in display area
+function listAccounts(){
+    let url = uri + "/" + currentUser.id + "/accounts"
+    cl(url)
+    apiCall(url, displayElements )
+    
+}
+//append elements to display area
+function displayElements(response){
+    let ele
+    //empty element
+    $('#display').empty()
+    //map throu all elements and create html string
+    response.map( obj=>{
+                    cl(obj)
+                    ele += "<div id='response' style='border:blue 1px solid'>"
+                    for(x in obj){
+                        if(x == "type"){
+                            ele +="<div> Account name:    " + obj[x] + "</div>"
+                        }
+                        if(x == "number"){
+                            ele +="<div> Account number:    " + obj[x] + "</div>"
+                        }
+                        if(x == "sortCode"){
+                            ele +="<div> Account sort code:   " + obj[x] + "</div>"
+                        }
+                        
+                    }
+                    ele +="<hr/></div>"
+                })
+    //append
+    document.getElementById("display").innerHTML = ele;
+}
+
+// ------------------------ CREATE ACCOUNT
+//listener
+$('#createAccount').click( createAccount )
+
+function createAccount(){
+    //define uri
+    uri="http://localhost:49000/banking/customers"
+    //construct url to be passed in
+    let url = uri + "/" + currentUser.id + "/accounts"
+    //get name value
+    let accountName = $('#accountName').val()
+    //if name empty
+    if(accountName==""){alert('enter name'); return}
+    //data to be sent
+    let type = '{\"type\": \"' + accountName + '\"\n    }'
+    //adjust ajax options 
+    settings.method = "POST"
+    settings.headers["Content-Type"]="application/json"
+    settings.processData = false
+    settings.data = type
+    cl('passed in url' + url)
+    //api call
+    apiCall(url, cl)
+    //reset settings
+    resetSettings()
+}
+
+
+
+
+
+
+
+/*
 
 
 // SEND API CALL
@@ -112,7 +193,6 @@ function send(uri){
             document.getElementById("display").innerHTML = ele;
         })
 }
-/*
 
 function send(uri){
     $.ajax(settings)
