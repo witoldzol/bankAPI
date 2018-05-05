2c9258e43ba1418754c03cd79572512e89652704
witoldzol <blue3656@gmail.com>
2018-05-05 19:30:39 +0100

    added CREATE new user functionality

--- client_bankAPI/script.js
+++ client_bankAPI/script.js
@@ -8,20 +8,22 @@ let ele
 let settings = {
   "async": true,
   "crossDomain": true,
-  //"url": "http://localhost:49000/banking/customers",
   "url": uri,
   "method": "GET",
   "headers": {
     "Cache-Control": "no-cache",
   }
 }
+
+
 //standarized api call, takes path to resource and function to execute on api response
 function apiCall(uri, method){
     $.ajax(settings)
         .done(response=>{method(response)})
 }
 
-
+// -----------------------------   LOGIN
+//listener
 $('#login').click( login )
 
 function login(uri){
@@ -34,18 +36,68 @@ function login(uri){
 }
 // function that return map that traverses json and compares input to values
 function mapThrough(response){
+    cl('from map throu function ' + response)
+    cl(response.loginName)
      response.map( obj=>{
                     if(obj.loginName == userName && obj.password == password){
                         enter(obj)
                     }
                 }) 
 }
-
+//make login forms visible upon logging in
 function enter(obj){
     $('#inputsForm').css("visibility", "visible")
     $('#greeting').html('welcome ' + obj.name)
 }
 
+// ----------------   CREATE NEW USER
+//listener
+$('#createUser').click(newUserMakeFormsVisible)
+$('#submitNewUser').click(createNewUser)
+
+function newUserMakeFormsVisible(){
+    $('#newUserinputsForm').css("visibility", "visible")    
+}
+//create new user
+function createNewUser(){
+    //get data from input forms
+    let name = $('#newUserName').val()
+    let address = $('#newUserAddress').val()
+    let loginName = $('#newUserLogin').val()
+    let password = $('#newUserPassword').val()
+    let customer = '{\t\n\t\"name\":\"' + name + '\",\n\t\"address\":\"' + address + '\",\n\t\"loginName\":\"' + loginName + '\",\n\t\"password\":\"' + password + '\"\n\t\n}'
+    //adjust ajax options 
+    settings.method = "POST"
+    settings.headers["Content-Type"]="application/json"
+    settings.processData = false
+    settings.data = customer
+    //api call
+    apiCall(uri, cl)
+    //clear fields
+    $('#newUserName').val('')
+    $('#newUserAddress').val('')
+    $('#newUserLogin').val('')
+    $('#newUserPassword').val('')
+    //login
+    $('#loginName').val(loginName)
+    $('#password').val(password)
+    
+    //reset settings
+    settings ={}
+    settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": uri,
+      "method": "GET",
+      "headers": {
+        "Cache-Control": "no-cache",
+      }
+    }
+
+}
+
+
+// SEND API CALL
 function send(uri){
     $.ajax(settings)
         .done( response=> {
